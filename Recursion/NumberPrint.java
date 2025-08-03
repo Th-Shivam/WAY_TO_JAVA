ed8fcfdbe4df22aabd58acfb2b6bb94357a6b123
th-shivam <dreamyshivam01@gmail.com>
2025-08-03 20:37:25 +0530

    First recursion program in java .

---
+++ Recursion/NumberPrint.java
@@ -0,0 +1,17 @@
+package Recursion;
+public class NumberPrint {
+    public static void printNum(int n){
+        if(n==0){
+            return ;
+        }
+
+        System.out.println(n);
+        printNum(n-1);
+    }    
+
+    public static void main(String[] args) {
+        int n = 5; 
+        System.out.println("Printing numbers from " + n + " to 1:");
+        printNum(n);
+    }
+}
