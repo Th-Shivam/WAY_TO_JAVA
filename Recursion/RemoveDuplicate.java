909494aa5731a878c725a372518a882018b00537
th-shivam <dreamyshivam01@gmail.com>
2025-08-09 10:33:11 +0530

    Remove duplicates in a string using recursion .

---
+++ Recursion/RemoveDuplicate.java
@@ -0,0 +1,32 @@
+class RemoveDuplicate {
+
+    public static boolean[] map = new boolean[26];
+
+    public static void RemoveDup(String str, String newStr, int idx) {
+        char currChar = str.charAt(idx);
+
+        if (idx == str.length() - 1) {
+            if (map[currChar - 'a']) {
+                System.out.println(newStr);
+            } else {
+                map[currChar - 'a'] = true;
+                newStr += currChar;
+                System.out.println(newStr);
+            }
+            return;
+        }
+
+        if (map[currChar - 'a']) {
+            RemoveDup(str, newStr, idx + 1);
+        } else {
+            map[currChar - 'a'] = true;
+            newStr += currChar;
+            RemoveDup(str, newStr, idx + 1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        RemoveDup("aaabaaaccccdeaaafbbbbgccc", "", 0);
+    }
+}
