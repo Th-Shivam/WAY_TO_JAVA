7d7d85a510807d283bca2ee76838eb1afd01c253
th-shivam <dreamyshivam01@gmail.com>
2025-08-10 21:21:19 +0530

    Updated factorial program .

--- Exercise-1/Factorial.java
+++ Exercise-1/Factorial.java
@@ -1,31 +1,33 @@
 import java.util.*;
+
 public class Factorial {
 
-    public static int Fact(int n ){
-        int fact = 1 ;
-        if(n==0){
+    public static int Fact(int n) {
+        int fact = 1;
+        if (n == 0) {
             System.out.println("Factorial of 0 is 1");
             return 1;
-            
+
         }
-        if(n<0){
+        if (n < 0) {
             System.out.println("Factorial of negative number is not defined");
             return -1;
-            
+
         }
 
-        for(int i=n ; i>=1 ; i--){
-             fact *= i ; }
+        for (int i = n; i >= 1; i--) {
+            fact *= i;
+        }
 
-             return fact;
+        return fact;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number to find its factorial : ");
         int n = sc.nextInt();
         int result = Fact(n);
-        if(result == -1) {
+        if (result == -1) {
             System.out.println("Cannot compute factorial for negative numbers.");
             sc.close();
             return;
@@ -34,5 +36,5 @@ public class Factorial {
         }
         sc.close();
     }
-    
+
 }
