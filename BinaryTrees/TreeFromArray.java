5c764d263336837f20b5323afcf9d0efc74438b4
th-shivam <dreamyshivam01@gmail.com>
2025-09-26 14:39:54 +0530

    Fixed errors due to local linkedlist files

--- BinaryTrees/TreeFromArray.java
+++ BinaryTrees/TreeFromArray.java
@@ -1,52 +1,85 @@
-import java.util.* ;
-class Node{
-    int data ;
-    Node left ;
-    Node right ;
-    Node(int data){
-        this.data = data ;
-        this.left = null ;
-        this.right = null ;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
 }
 
-class BinaryTree{
-    int idx = -1 ;
-    public Node buildTree(int nodes[]){
-        idx++ ;
-        if(nodes[idx] == -1){
-            return null ;
+class BinaryTree {
+    int idx = -1;
+
+    public Node buildTree(int nodes[]) {
+        idx++;
+        if (nodes[idx] == -1) {
+            return null;
         }
         Node newNode = new Node(nodes[idx]);
         newNode.left = buildTree(nodes);
         newNode.right = buildTree(nodes);
-        return newNode ;
+        return newNode;
     }
 }
 
 public class TreeFromArray {
 
-
-        public static int countNodes(Node root){
-            if (root == null){
-                return 0 ;
-            }
-
-            return countNodes(root.left) + countNodes(root.right) + 1 ;
+    public static int countNodes(Node root) {
+        if (root == null) {
+            return 0;
         }
 
-        public static int sumNodes(Node root){
-            if (root == null){
-                return 0 ;
-            }
+        return countNodes(root.left) + countNodes(root.right) + 1;
+    }
 
-            return sumNodes(root.left) + sumNodes(root.right) + root.data ;
+    public static int sumNodes(Node root) {
+        if (root == null) {
+            return 0;
         }
 
-        public static void main(String[] args) {
-        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1} ;
+        return sumNodes(root.left) + sumNodes(root.right) + root.data;
+    }
 
-        BinaryTree tree = new BinaryTree() ;
+    public void LevelOrder(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(null);
+
+        while (!queue.isEmpty()) {
+            Node currNode = queue.poll();
+            if (currNode == null) {
+                System.out.println();
+                if (queue.isEmpty()) {
+                    break;
+                } else {
+                    queue.add(null);
+                }
+            } else {
+                System.out.print(currNode.data + " ");
+                if (currNode.left != null) {
+                    queue.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    queue.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+
+        BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         System.out.println(root.data);
         System.out.println("No. of nodes : " + countNodes(root));
