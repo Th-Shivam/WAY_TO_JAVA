83643a3d509dcb292959a096fc85a2d0b38d0261
th-shivam <dreamyshivam01@gmail.com>
2025-11-03 16:00:07 +0530

    Inverse of a given number .

--- PepCoding/Inverseofanum.java
+++ PepCoding/Inverseofanum.java
@@ -1,21 +1,22 @@
 package PepCoding;
-
+import java.util.* ;
 class Inverseofanum {
     public static void main(String[] args) {
-        int n = 21453 ;
+        Scanner sc = new Scanner(System.in) ;
+        int n = sc.nextInt() ;
         int pos = 1 ;
         int inverse = 0 ;
         while(n != 0 ){
             int od = n % 10 ;
             int id = pos ;
-            int ip = od ;
-            
-            
+            int ip = od ; 
             n /= 10 ;
             inverse = inverse + id * (int)Math.pow(10 , ip - 1 ) ;
             pos++ ;
         }
         
         System.out.println(inverse) ;
+        sc.close();
     }
+    
 }
