9e0dd97d53760a7fa3748cd9e2b0958c0dc30fe6
th-shivam <dreamyshivam01@gmail.com>
2025-11-04 20:57:48 +0530

    Rotate a given number .

---
+++ PepCoding/rotatenum.java
@@ -0,0 +1,28 @@
+package PepCoding;
+
+import java.util.* ;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        System.out.println("Enter the original number :") ;
+        int n = sc.nextInt(); //input og num 
+        String strNum = String.valueOf(n); // converting to string 
+        int size = strNum.length() ; //calculated length 
+        System.out.println("Enter the rotation number :") ;
+        int k = sc.nextInt();
+        if(k>size){
+            k = k%size ;
+        }
+        if(k<0){
+            k = k + size ;
+        }
+        int div = (int)Math.pow(10,k ) ;
+        
+        int rotatedNum = 0 ;
+        int rem = n % div ;
+        n /= div ;
+        rotatedNum = rem * (int)Math.pow(10,size-k) + n ;
+        System.out.println(rotatedNum) ;
+        sc.close() ;
+    }
+}
