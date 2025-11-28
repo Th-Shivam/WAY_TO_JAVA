f3cfde0317f533850fef300535930874afc13a05
Shivam Singh <dreamyshivam01@gmail.com>
2025-11-28 23:14:12 +0530

    Create DNF.java

---
+++ Array/DNF.java
@@ -0,0 +1,38 @@
+public class DNFSort {
+    public static void sortColors(int[] arr) {
+        int low = 0, mid = 0, high = arr.length - 1;
+
+        while (mid <= high) {
+            switch (arr[mid]) {
+                case 0:
+                    int temp0 = arr[low];
+                    arr[low] = arr[mid];
+                    arr[mid] = temp0;
+                    low++;
+                    mid++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    int temp2 = arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high] = temp2;
+                    high--;
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 0, 1, 2, 1, 0};
+        
+        sortColors(arr);
+        
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
