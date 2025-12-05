ce75f8e7f894411fd48d501c392422681bbd3575
Shivam Singh <dreamyshivam01@gmail.com>
2025-12-05 23:58:53 +0530

    Create 123.java

---
+++ Array/123.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> poss_j = new Stack<>();
+        int k = Integer.MIN_VALUE;
+        for(int i=nums.length-1; i>=0; i--){
+            if(nums[i]<k) return true;
+            while(!poss_j.isEmpty()&& nums[i]>poss_j.peek()){
+                k = poss_j.pop();
+            }
+            poss_j.push(nums[i]);
+        }
+        return false;
+    }
+}
