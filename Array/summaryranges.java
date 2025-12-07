05c73162b9a422b863ee8d6f172e8dcd176f9d17
Shivam Singh <dreamyshivam01@gmail.com>
2025-12-07 23:18:36 +0530

    Create summaryranges.java

---
+++ Array/summaryranges.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if(nums.length == 0) return result;
+
+        int a = nums[0];
+        int b = nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] == nums[i-1]+1)
+                b = nums[i];
+            else{
+                result.add(range(a,b));
+                a = nums[i];
+                b = nums[i];
+            }
+        }
+        result.add(range(a,b));
+
+        return result;
+    }
+
+    private String range(int a,int b){
+        return (a == b) ? String.valueOf(a) : a + "->" + b;
+    }
+}
