cdf9eea6c364f87a1a9e3d345be2ccc07463308f
Shivam Singh <dreamyshivam01@gmail.com>
2025-12-09 23:44:38 +0530

    Create Arrayproductexceptself.java

---
+++ Array/Arrayproductexceptself.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int mul=1;
+        int zeroindex=-1;
+        boolean zerofound=false;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0&&(!zerofound)){
+                zerofound=!zerofound;
+                zeroindex=i;
+                continue;
+            }
+            mul*=nums[i];
+        }
+        if(zerofound){
+            Arrays.fill(nums,0);
+            nums[zeroindex]=mul;
+            return nums;
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=mul/nums[i];
+        }
+        return nums;
+    }
+}
