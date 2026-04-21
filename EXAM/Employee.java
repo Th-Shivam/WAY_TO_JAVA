b3542cfc77bd56b9a336d91de5d646b57fefba7e
th-shivam <dreamyshivam01@gmail.com>
2026-04-21 23:34:42 +0530

    Add employee model class

---
+++ EXAM/Employee.java
@@ -0,0 +1,29 @@
+public class Employee {
+    private String name;
+    private String id;
+    private String department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
