c8fdfd71453131997820e95cbec7213b3bf1209c
th-shivam <dreamyshivam01@gmail.com>
2026-04-21 23:34:28 +0530

    Add view component

---
+++ EXAM/View2.java
@@ -0,0 +1,7 @@
+public class View2 {
+    public static void printDetails(String name , String department , int id ){
+        System.out.println("Id = : " + id);
+        System.out.println("Name = : " + name);
+        System.out.println("department = : " + department);
+    }
+}
