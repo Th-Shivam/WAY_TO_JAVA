4c2c7c5f8ef4c02cf01b17e006a13650533327fe
th-shivam <dreamyshivam01@gmail.com>
2026-04-21 23:33:40 +0530

    Add border layout demo

---
+++ Practice/BorderLayoutDemo.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+class BorderLayoutDemo extends JFrame {
+
+    BorderLayoutDemo() {
+        setLayout(new BorderLayout());
+
+        add(new JButton("North"), BorderLayout.NORTH);
+        add(new JButton("South"), BorderLayout.SOUTH);
+        add(new JButton("East"), BorderLayout.EAST);
+        add(new JButton("West"), BorderLayout.WEST);
+        add(new JButton("Center"), BorderLayout.CENTER);
+
+        setTitle("BorderLayout Demo");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new BorderLayoutDemo();
+    }
+}
