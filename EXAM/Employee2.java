e2b9ed6cf5f96c75b15c422a11f032cd5037d258
th-shivam <dreamyshivam01@gmail.com>
2026-04-21 23:34:15 +0530

    Add employee class implementation

---
+++ EXAM/Employee2.java
@@ -0,0 +1,34 @@
+public class Employee2 {
+
+    private String name;
+    private int id;
+    private String department;
+    
+    
+    public void setName(String name) {
+        this.name =  name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department ;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+}
