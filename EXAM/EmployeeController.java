72ba51fb69c8c16f65afb3bbdf16ddf79bc42dac
th-shivam <dreamyshivam01@gmail.com>
2026-04-21 23:34:30 +0530

    Add employee controller for MVC

---
+++ EXAM/EmployeeController.java
@@ -0,0 +1,21 @@
+public class EmployeeController {
+    private Employee model;
+    private EmployeeView view;
+
+    public EmployeeController(Employee model, EmployeeView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setEmployeeName(String name) {
+        model.setName(name);
+    }
+
+    public String getEmployeeName() {
+        return model.getName();
+    }
+
+    public void updateView() {
+        view.printEmployeeDetails(model.getName(), model.getId(), model.getDepartment());
+    }
+}
