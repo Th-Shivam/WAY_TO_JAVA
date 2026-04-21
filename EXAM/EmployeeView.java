4fb84fa30db1b2a4273e360d4cddc9289b843b3e
th-shivam <dreamyshivam01@gmail.com>
2026-04-21 23:34:18 +0530

    Add employee view for MVC

---
+++ EXAM/EmployeeView.java
@@ -0,0 +1,8 @@
+public class EmployeeView {
+    public void printEmployeeDetails(String name, String id, String department) {
+        System.out.println("Employee Details:");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Department: " + department);
+    }
+}
